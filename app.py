bcf57c9fe7bf9ef279f9359ec61b13bddc7629a4
Apoorv Toke <81573506+apoorvtoke@users.noreply.github.com>
2021-09-08 23:52:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@
 # from sklearn.externals import joblib
 from flask import Flask, request, render_template
 import numpy as np
-from keras.models import load_model
-import h5py
+# from keras.models import load_model
+# import h5py
 
 # Create Flask object to run
 app = Flask(__name__)
 
 # Load the model from the file
-iris_model = load_model('eye_for_blind.hdf5')
+# iris_model = load_model('eye_for_blind.hdf5')
 
 @app.route('/')
 def home():
@@ -24,18 +24,8 @@ def predict():
     int_features = [ int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = client_model.predict(final_features)
-    
     # output = round(prediction[0], 2)
     output = prediction
-    ##############################################################################
-    # img_path = request.args['img_path']    
-    # print(img_path)
-
-    # test_inp = np.array([img_path]).reshape(1, 1)
-    # class_predicted = int(iris_model.predict(test_inp)[0])
-    # output = "Predicted Eye for blind Class: " + str(class_predicted)
-    ##############################################################################
-
     return (output)
 
 
